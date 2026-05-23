67f1e6d1ff09dd8fcc12bb5d965d3547b5bb3b73
ssch71 <s01093450498@gmail.com>
2026-05-23 16:38:22 +0900

    xt_connmark.h 업데이트

--- include/linux/netfilter/xt_connmark.h
+++ include/linux/netfilter/xt_connmark.h
@@ -4,9 +4,25 @@
 
 #include <linux/types.h>
 
+enum {
+	XT_CONNMARK_SET = 0,
+	XT_CONNMARK_SAVE,
+	XT_CONNMARK_RESTORE,
+};
+
+enum {
+	D_SHIFT_LEFT = 0,
+	D_SHIFT_RIGHT,
+};
+
 struct xt_connmark_tginfo1 {
 	__u32 ctmark, ctmask, nfmask;
 	__u8 mode;
+};
+
+struct xt_connmark_tginfo2 {
+	__u32 ctmark, ctmask, nfmask;
+	__u8 shift_dir, shift_bits, mode;
 };
 
 struct xt_connmark_mtinfo1 {
